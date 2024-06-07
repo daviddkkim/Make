051d78040ee23c108790012e2dd70c77f8f7d60b
daviddkkim <david.kim@richmond.edu>
2024-06-07 12:40:20 -0400

    adding grayscale

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -63,6 +63,54 @@ const config: Config = {
         light: "#0f296f",
         dark: "#d0e2ff"
       },
+      "slate-100": {
+        light: "#fcfcfd",
+        dark: "#111113"
+      },
+      "slate-200": {
+        light: "#f9f9fb",
+        dark: "#18191b"
+      },
+      "slate-300": {
+        light: "#f0f0f3",
+        dark: "#212225"
+      },
+      "slate-400": {
+        light: "#e8e8ec",
+        dark: "#272a2d"
+      },
+      "slate-500": {
+        light: "#e0e1e6",
+        dark: "#2e3135"
+      },
+      "slate-600": {
+        light: "#d9d9e0",
+        dark: "#363a3f"
+      },
+      "slate-700": {
+        light: "#cdced6",
+        dark: "#43484e"
+      },
+      "slate-800": {
+        light: "#b9bbc6",
+        dark: "#5a6169"
+      },
+      "slate-900": {
+        light: "#8b8d98",
+        dark: "#696e77"
+      },
+      "slate-1000": {
+        light: "#80838d",
+        dark: "#777b84"
+      },
+      "slate-1100": {
+        light: "#60646c",
+        dark: "#b0b4ba"
+      },
+      "slate-1200": {
+        light: "#1c2024",
+        dark: "#edeef0"
+      },
     }
   },
   plugins: [],
