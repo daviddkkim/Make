c9d43e070a4195b2e8b3d7ab0b43a84eecf9d0fc
daviddkkim <david.kim@richmond.edu>
2024-06-07 22:43:15 -0400

    fix tailwind config issue and change grayscale

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -13,105 +13,106 @@ const config: Config = {
         "gradient-conic":
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
+      colors: {
+        "primary-100": {
+          light: "#fcfdff",
+          dark: "#091021"
+        },
+        "primary-200": {
+          light: "#f5faff",
+          dark: "#0d172c"
+        },
+        "primary-300": {
+          light: "#eaf2ff",
+          dark: "#0c2158"
+        },
+        "primary-400": {
+          light: "#ddeaff",
+          dark: "#0c2977"
+        },
+        "primary-500": {
+          light: "#ccdfff",
+          dark: "#11328d"
+        },
+        "primary-600": {
+          light: "#b6d1ff",
+          dark: "#193e9f"
+        },
+        "primary-700": {
+          light: "#97beff",
+          dark: "204ab6"
+        },
+        "primary-800": {
+          light: "#72a3ff",
+          dark: "#2556d6"
+        },
+        "primary-900": {
+          light: "#0300e0",
+          dark: "#0300e0"
+        },
+        "primary-1000": {
+          light: "#32009b",
+          dark: "#32009b"
+        },
+        "primary-1100": {
+          light: "#164df3",
+          dark: "#89b1ff"
+        },
+        "primary-1200": {
+          light: "#0f296f",
+          dark: "#d0e2ff"
+        },
+        "sand-100": {
+          light: "#fdfdfc",
+          dark: "#111110"
+        },
+        "sand-200": {
+          light: "#f9f9f8",
+          dark: "#191918"
+        },
+        "sand-300": {
+          light: "#f1f0ef",
+          dark: "#222221"
+        },
+        "sand-400": {
+          light: "#e9e8e6",
+          dark: "#2a2a28"
+        },
+        "sand-500": {
+          light: "#e2e1de",
+          dark: "#31312e"
+        },
+        "sand-600": {
+          light: "#dad9d6",
+          dark: "#3b3a37"
+        },
+        "sand-700": {
+          light: "#cfceca",
+          dark: "#494844"
+        },
+        "sand-800": {
+          light: "#bcbbb5",
+          dark: "#62605b"
+        },
+        "sand-900": {
+          light: "#8d8d86",
+          dark: "#6f6d66"
+        },
+        "sand-1000": {
+          light: "#82827c",
+          dark: "#7c7b74"
+        },
+        "sand-1100": {
+          light: "#63635e",
+          dark: "#b5b3ad"
+        },
+        "sand-1200": {
+          light: "#21201c",
+          dark: "#eeeeec"
+        },
+      }
     },
-    colors: {
-      "primary-100": {
-        light: "#fcfdff",
-        dark: "#091021"
-      },
-      "primary-200": {
-        light: "#f5faff",
-        dark: "#0d172c"
-      },
-      "primary-300": {
-        light: "#eaf2ff",
-        dark: "#0c2158"
-      },
-      "primary-400": {
-        light: "#ddeaff",
-        dark: "#0c2977"
-      },
-      "primary-500": {
-        light: "#ccdfff",
-        dark: "#11328d"
-      },
-      "primary-600": {
-        light: "#b6d1ff",
-        dark: "#193e9f"
-      },
-      "primary-700": {
-        light: "#97beff",
-        dark: "204ab6"
-      },
-      "primary-800": {
-        light: "#72a3ff",
-        dark: "#2556d6"
-      },
-      "primary-900": {
-        light: "#0300e0",
-        dark: "#0300e0"
-      },
-      "primary-1000": {
-        light: "#32009b",
-        dark: "#32009b"
-      },
-      "primary-1100": {
-        light: "#164df3",
-        dark: "#89b1ff"
-      },
-      "primary-1200": {
-        light: "#0f296f",
-        dark: "#d0e2ff"
-      },
-      "slate-100": {
-        light: "#fcfcfd",
-        dark: "#111113"
-      },
-      "slate-200": {
-        light: "#f9f9fb",
-        dark: "#18191b"
-      },
-      "slate-300": {
-        light: "#f0f0f3",
-        dark: "#212225"
-      },
-      "slate-400": {
-        light: "#e8e8ec",
-        dark: "#272a2d"
-      },
-      "slate-500": {
-        light: "#e0e1e6",
-        dark: "#2e3135"
-      },
-      "slate-600": {
-        light: "#d9d9e0",
-        dark: "#363a3f"
-      },
-      "slate-700": {
-        light: "#cdced6",
-        dark: "#43484e"
-      },
-      "slate-800": {
-        light: "#b9bbc6",
-        dark: "#5a6169"
-      },
-      "slate-900": {
-        light: "#8b8d98",
-        dark: "#696e77"
-      },
-      "slate-1000": {
-        light: "#80838d",
-        dark: "#777b84"
-      },
-      "slate-1100": {
-        light: "#60646c",
-        dark: "#b0b4ba"
-      },
-      "slate-1200": {
-        light: "#1c2024",
-        dark: "#edeef0"
-      },
-    }
+
   },
   plugins: [],
 };
