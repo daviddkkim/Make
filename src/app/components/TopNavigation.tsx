63f4d624b9449c5528a6cdabc6c3f4de00b567ec
daviddkkim <david.kim@richmond.edu>
2024-06-21 14:29:36 -0400

    new toast component + style

--- src/app/components/TopNavigation.tsx
+++ src/app/components/TopNavigation.tsx
@@ -9,7 +9,7 @@ const TopNavigation = () => {
   const segment = useSelectedLayoutSegment();
 
   return (
-    <nav className="p-2 border-r bg-stone-100 w-full justify-between flex items-center text-stone-100 border-b sticky top-0">
+    <nav className="p-2 border-r bg-stone-100 w-full justify-between flex items-center text-stone-100 border-b sticky top-0 z-10">
       <div className="flex gap-2 items-center">
         <Link
           className="font-mono px-2 w-fit h-[24px] bg-[#0300E0] rounded-md text-xs flex items-center justify-center text-stone-100 mr-6"
