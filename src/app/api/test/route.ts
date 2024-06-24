d2f2375f90e67783ce4ac2c1b3b4bec0ca3ddff3
daviddkkim <david.kim@richmond.edu>
2024-06-24 17:03:55 -0400

    remove unused api

--- src/app/api/test/route.ts
+++
@@ -1,8 +0,0 @@
-import type { NextApiRequest, NextApiResponse } from "next";
-
-export async function GET(req: NextApiRequest, res: NextApiResponse) {
-  console.log(req.headers);
-  const token = req.headers.authorization;
-  console.log(token);
-  return Response.json({ token });
-}
